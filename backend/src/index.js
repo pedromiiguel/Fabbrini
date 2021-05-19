b79532a6e4cf82c1b3d340b1e4ac55ce921ada57
Pedro Miguel <pedromrap@gmail.com>
2021-05-19 10:11:17 -0300

    add environment variables

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -10,13 +11,11 @@ app.use(routes)
 
 //Models: são representação de entidades da nossa aplicação
 //Conexão com o MongoDB
-mongoose.connect('', { 
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cursojs.e2nu7.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, { 
   useNewUrlParser: true,
   useUnifiedTopology: true,
 
 });
-
-
 
 //Query Params: req.query (Filtros, Ordenação, paginação) http://localhost:3333/users?name=Pedro
 //Route Params: req.params (Identificar um recurso na alteração ou remoção "/:id")
